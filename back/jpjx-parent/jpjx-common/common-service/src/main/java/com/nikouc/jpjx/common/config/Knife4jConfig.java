9d24242f019f29189647b78d45146d3ccb5582a6
nikou <1286493714@qq.com>
2024-07-02 01:12:07 +0800

    Kinfe4j配置类

---
+++ back/jpjx-parent/jpjx-common/common-service/src/main/java/com/nikouc/jpjx/common/config/Knife4jConfig.java
@@ -0,0 +1,33 @@
+package com.nikouc.jpjx.common.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Knife4jConfig {
+    @Bean
+    public GroupedOpenApi adminApi(){
+        return GroupedOpenApi.builder()
+                .group("admin-api")
+                .pathsToMatch("/admin/**")
+                .build();
+    }
+
+    /**
+     * @description 自定义接口信息
+     */
+    @Bean
+    public OpenAPI customOpenAPI(){
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("佳品精选API接口文档")
+                        .version("1.0")
+                        .description("佳品精选API接口文档")
+                        .contact(new Contact().name("nikouc")));//作者
+    }
+}
